b3cfb449f109eee4a54bb2f01b1ffe9a02e0baf8
isagarjaiwal <sagarjaiswal815555@gmail.com>
2024-03-02 15:20:32 +0530

    completed

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const apiUrl = process.env.REACT_APP_API_URL;
+// const apiUrl = process.env.REACT_APP_API_URL;
+const apiUrl = "http://localhost:5500/";
 
 export async function addData(data) {
   try {
@@ -13,7 +14,7 @@ export async function addData(data) {
 
 export async function updateData(data) {
   try {
-    const response = await axios.put(`${apiUrl}api/update`, data);
+    const response = await axios.post(`${apiUrl}api/update`, data);
   } catch (error) {
     console.error("Error updating data:", error.response.data);
   }
--- src/components/count/countCard.jsx
+++ src/components/count/countCard.jsx
@@ -14,7 +14,6 @@ const CountCard = ({ isUpdated }) => {
         console.error("Error fetching counts:", error);
       }
     }
-
     fetchData();
   }, [isUpdated]);
 
