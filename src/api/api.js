5580db61bffb3657dffe5a4f33f60078edd347d9
isagarjaiwal <sagarjaiswal815555@gmail.com>
2024-03-02 11:17:58 +0530

    commit with comments

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-// const apiUrl = process.env.REACT_APP_API_URL;
+const apiUrl = process.env.REACT_APP_API_URL;
 // Function to add data
 export async function addData(data) {
   try {
-    const response = await axios.post("https://resizable-be.onrender.com/api/add", data);
+    const response = await axios.post(apiUrl + "api/add", data);
     return response.status;
   } catch (error) {
     console.error("Error adding data:", error); // Log error with clear message
@@ -13,7 +13,7 @@ export async function addData(data) {
 // Function to update data
 export async function updateData(data) {
   try {
-    const response = await axios.put("https://resizable-be.onrender.com/api/update", data);
+    const response = await axios.put(apiUrl + "api/update", data);
     console.log({ a: response.data }); // Log response data for debugging purposes
   } catch (error) {
     console.error("Error updating data:", error.response.data); // Log error with clear message and include response data
@@ -23,7 +23,7 @@ export async function updateData(data) {
 // Function to get counts
 export async function getCounts() {
   try {
-    const response = await axios.get("https://resizable-be.onrender.com/api/count");
+    const response = await axios.get(apiUrl + "api/count");
     console.log(response.data); // Log counts from the server for debugging purposes
     return response.data; // Return counts from the server
   } catch (error) {
@@ -34,7 +34,7 @@ export async function getCounts() {
 // Function to get all data
 export async function getAllData() {
   try {
-    const response = await axios.get("https://resizable-be.onrender.com/api/allEntries");
+    const response = await axios.get(apiUrl + "api/allEntries");
     console.log(response.data); // Log all entries from the server for debugging purposes
     return response.data; // Return all entries from the server
   } catch (error) {
