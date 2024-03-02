8dee8bee5a09cefe57b5b62e3f3b02c55c2198d7
isagarjaiwal <sagarjaiswal815555@gmail.com>
2024-03-02 14:47:12 +0530

    completed

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const apiUrl = process.env.PORT;
+
+const apiUrl = process.env.REACT_APP_API_URL;
 
 export async function addData(data) {
   try {
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import replace from '@rollup/plugin-replace';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    replace({
+      'process.env.REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_API_URL),
+    }),
+  ],
+});
